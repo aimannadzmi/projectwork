35a7c46183de068712bab43e597f15afc39f8f8b
andre2656 <44176906+andre2656@users.noreply.github.com>
2018-12-31 18:16:01 -0700

    New Branch Has UserProfile and Nav scores

--- odds.js
+++ odds.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     hidebuttons($('#currentGames'), true)
 
     var nbaBtn = ['Lakers', 'Suns', 'Knicks', 'Pacers', 'Spurs', 'Rockets', 'Warriors', 'Kings', 'Clippers', 'Thunder', 'Wizards', 'Timberwolves', 'Hornets', 'Jazz', 'Nets', 'Pistons', 'Trail_Blazers', 'Pelicans', 'Hawks', 'Grizzlies', 'Magic', 'Bucks', 'Bulls', 'Maverics', '76ers', 'Cavaliers', 'Celtics', 'Raptors', 'Nuggets', 'Heat'];
-    var nbaCities = ['Los_Angeles_', 'Pheonix_', 'New_York_', 'Indiana_', 'San_Antonio_', 'Houston_', 'Golden_State_', 'Sacramento_', 'Los_Angeles_', 'Oklahoma_City_', 'Washington_', 'Minnisota_', 'Charlotte_', 'Utah_', 'New_Jersey_', 'Detroit_', 'Portland_', 'New_Orleans', 'Atlanta_', 'Memphis_', 'Orlando_', 'Milwaukee_', 'Chicago_', 'Dallas_', 'Philadelphia_', 'Clevland', 'Boston_', 'Toronto_', 'Denver_', 'Miami_']
+    var nbaCities = ['Los_Angeles_', 'Phoenix_', 'New_York_', 'Indiana_', 'San_Antonio_', 'Houston_', 'Golden_State_', 'Sacramento_', 'Los_Angeles_', 'Oklahoma_City_', 'Washington_', 'Minnisota_', 'Charlotte_', 'Utah_', 'New_Jersey_', 'Detroit_', 'Portland_', 'New_Orleans', 'Atlanta_', 'Memphis_', 'Orlando_', 'Milwaukee_', 'Chicago_', 'Dallas_', 'Philadelphia_', 'Clevland', 'Boston_', 'Toronto_', 'Denver_', 'Miami_']
     function appendOptions(array, array2, appendHere) {
         for (var i = 0; i < array.length; i++) {
             //append the option
@@ -19,31 +19,25 @@ $(document).ready(function () {
     //when submit button is clicked
     $("#submit").on("click", function () {
         event.preventDefault();
-
         var input = $("#searchteams").val();
-
         console.log(input);
-
         $("#searchteams").val("");
-
         //appendTeams();
         //appendScores();
-
-
-    })
- $('#defaultGames')
- $('#currentGames')
+    });
+    $('#defaultGames');
+    $('#currentGames');
 
     function hidebuttons(selectedGroup, question) {
         if (question === true) {
-            console.log("Hiding- "+ selectedGroup)
+            console.log("Hiding- " + selectedGroup)
             selectedGroup.hide()
         } else if (question === false) {
             console.log("Showing- " + selectedGroup)
             selectedGroup.show()
         }
-    }
-    
+    };
+
 
     function defaultGames() {
 
@@ -63,7 +57,7 @@ $(document).ready(function () {
                 var game2 = response3.events[i].strFilename;
                 var home2 = response3.events[i].strHomeTeam + ": " + response3.events[i].intHomeScore;
                 var away2 = response3.events[i].strAwayTeam + ": " + response3.events[i].intAwayScore;
-                newGame2.html('<br>'+ game2 + '<br>' + home2 + '<br>' + away2);
+                newGame2.html('<br>' + game2 + '<br>' + home2 + '<br>' + away2);
 
                 $('#defaultGames').append(newGame2)
             }
@@ -72,37 +66,36 @@ $(document).ready(function () {
 
 
 
-    }    
-defaultGames();    
-    
-    
+    }
+    defaultGames();
+
+
 
 
     function getTeamId() {
-        $('#submitTeam').on('click', function(){
+        $('#submitTeam').on('click', function () {
             hidebuttons($('#defaultGames'), true);
             hidebuttons($('#currentGames'), false);
             var teamName = $('#teamSel').val().trim();
             localStorage.setItem('teamSelected', teamName);
-            
+
             console.log("local Storage: " + localStorage.getItem('teamSelected'));
             var queryURL = 'https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=' + localStorage.getItem('teamSelected');
-       
-        
-        
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-            
-            var teamId = response.teams[0].idTeam;
-            console.log(response.teams[0].idTeam)
-            console.log(teamId)
-            pullGames(teamId);
-        })
+
+
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+                console.log(response);
+
+                var teamId = response.teams[0].idTeam;
+                console.log(response.teams[0].idTeam)
+                console.log(teamId)
+                pullGames(teamId);
+            })
         });
-        
+
 
     }
 
@@ -124,8 +117,8 @@ defaultGames();
                 var game = response2.results[i].strFilename;
                 var home = response2.results[i].strHomeTeam + ": " + response2.results[i].intHomeScore;
                 var away = response2.results[i].strAwayTeam + ": " + response2.results[i].intAwayScore;
-                newGame.html('<br>'+game + '<br>' + home + '<br>' + away);
-                
+                newGame.html('<br>' + game + '<br>' + home + '<br>' + away);
+
                 $('#currentGames').append(newGame)
             }
 
@@ -136,7 +129,7 @@ defaultGames();
     }
 
 
-    
+
 
 
     //betting odds api
@@ -151,14 +144,14 @@ defaultGames();
             var fetch = response.data
             console.log(response.data.length)
             for (var i = 0; i < response.data.length; i++) {
-                console.log('Sites.lenght: '+fetch[i].sites.length);
+                console.log('Sites.lenght: ' + fetch[i].sites.length);
                 var homeTeam = fetch[i].home_team;
                 var fetchSites;
-                
-                if (fetch[i].sites.length > 0){
+
+                if (fetch[i].sites.length > 0) {
                     fetchSites = fetch[i].sites[0].site_key;
                     console.log('fetch sites: ' + fetchSites)
-                }else{
+                } else {
                     continue
                 }
                 var fetchOdds = fetch[i].sites[0].odds.spreads.odds;
